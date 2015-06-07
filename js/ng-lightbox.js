fbf215f42f816c05928c4d02a4c07a9b0f81fae7
Ben Dougherty <ben@dougherty.net.au>
2015-06-07 16:15:10 +0800

    Tidy up some JS.

--- js/ng-lightbox.js
+++ js/ng-lightbox.js
@@ -3,44 +3,56 @@
   Drupal.behaviors.ng_lightbox = {
     attach: function (context, settings) {
 
-      var self = this;
-
-      // We don't actually use context which could cause issues?
-      $('.lightbox__overlay').once(function() {
-        // Close the lightbox.
-        $(this).click(function(e) {
-          if ($(e.target).is('.lightbox__overlay, .lightbox__header')) {
-            self.hide($(this).closest('.lightbox__overlay'));
-          }
-        });
-      });
-
-      // This isn't ideal as technically attachBehaviours could be called with
-      // document more than once but i'm not sure how to get around that?
-      if (context === document) {
-        $(document).keyup(function(e) {
-          // I couldn't bare to have a lightbox without a keyboard shortcut to
-          // close it.
-          if (e.which === 27) {
-            self.hide($('.lightbox__overlay'));
-          }
-        });
+      // Setup the first time.
+      if (!settings.ng_lightbox_init) {
+        this.initialize();
+        settings.ng_lightbox_init = true;
       }
+
+      var self = this;
+      $('.lightbox__overlay').click(function(e) {
+        if ($(e.target).is('.lightbox__overlay, .lightbox__header')) {
+          self.hide($(this).closest('.lightbox__overlay'));
+        }
+      });
     },
+
+    /**
+     * Hide the given lightbox overlay.
+     *
+     * @param $overlay
+     */
     hide: function($overlay) {
       // Remove the lock class on body.
       $('body').removeClass('lock');
       $overlay.addClass('ng-hidden');
+      $('#ng-lightbox').empty();
+    },
+
+    /**
+     * Initialize the lightbox the first time.
+     */
+    initialize: function() {
+      // Append an empty div with an id that we can use for appending our AJAX
+      // results to.
+      var $body = $('body');
+      $body.addClass('ng-lightbox-ready');
+      $body.append('<div id="ng-lightbox"></div>');
+
+      // Bind an event handler to document that hides all lightboxes.
+      var self = this;
+      $(document).keyup(function(e) {
+        // I couldn't bare to have a lightbox without a keyboard shortcut to
+        // close it.
+        if (e.which === 27) {
+          self.hide($('.lightbox__overlay'));
+        }
+      });
     }
   };
 
   Drupal.behaviors.ng_ajax_link = {
     attach: function (context, settings) {
-      var $body = $('body');
-      if (!$body.hasClass('ng-lightbox-ready')) {
-        $body.addClass('ng-lightbox-ready');
-        $body.append('<div id="ng-lightbox"></div>');
-      }
 
       $('.ng-lightbox:not(.ng-lightbox-processed)').addClass('ng-lightbox-processed').each(function () {
         var base = 'ng-lightbox';
